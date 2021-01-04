5558e3e9ad3f48003b894996cfb941e02a856f04
Sam Martin <broccoliboy@gmail.com>
2021-01-04 16:19:35 -0600

    add alternative start function with arguments for entry point and foreground options

--- pythonforandroid/bootstraps/common/build/templates/Service.tmpl.java
+++ pythonforandroid/bootstraps/common/build/templates/Service.tmpl.java
@@ -19,15 +19,19 @@ public class Service{{ name|capitalize }} extends PythonService {
     }
 
     static public void start(Context ctx, String pythonServiceArgument) {
+        start(ctx, pythonServiceArgument, "{{ entrypoint }}", "{{ foreground|lower }}");
+    }
+    
+    static public void start(Context ctx, String pythonServiceArgument, String serviceEntrypoint, String startForeground) {
         Intent intent = new Intent(ctx, Service{{ name|capitalize }}.class);
         String argument = ctx.getFilesDir().getAbsolutePath() + "/app";
         intent.putExtra("androidPrivate", ctx.getFilesDir().getAbsolutePath());
         intent.putExtra("androidArgument", argument);
         intent.putExtra("serviceTitle", "{{ args.name }}");
         intent.putExtra("serviceDescription", "{{ name|capitalize }}");
-        intent.putExtra("serviceEntrypoint", "{{ entrypoint }}");
+        intent.putExtra("serviceEntrypoint", serviceEntrypoint);
         intent.putExtra("pythonName", "{{ name }}");
-        intent.putExtra("serviceStartAsForeground", "{{ foreground|lower }}");
+        intent.putExtra("serviceStartAsForeground", startForeground);
         intent.putExtra("pythonHome", argument);
         intent.putExtra("pythonPath", argument + ":" + argument + "/lib");
         intent.putExtra("pythonServiceArgument", pythonServiceArgument);
