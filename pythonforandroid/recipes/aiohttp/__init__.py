0e160ea596468e2e127107decfb1cbc2f0f46f9d
Sam Martin <broccoliboy@gmail.com>
2021-01-06 08:55:00 -0600

    update aiohttp recipe to include deps

--- pythonforandroid/recipes/aiohttp/__init__.py
+++ pythonforandroid/recipes/aiohttp/__init__.py
@@ -6,11 +6,21 @@ from pythonforandroid.recipe import CythonRecipe  # type: ignore
 class AIOHTTPRecipe(CythonRecipe):  # type: ignore # pylint: disable=R0903
     """Build AIOHTTP"""
 
-    version = "v3.6.2"
+    version = "v3.7.3"
     url = "https://github.com/aio-libs/aiohttp/archive/{version}.zip"
     name = "aiohttp"
 
-    depends: List[str] = ["setuptools"]
+    depends = ["setuptools"]
+
+    python_depends = [
+        'attrs>=17.3.0',
+        'chardet>=2.0,<4.0',
+        'multidict>4.5,<7.0', 
+        'async_timeout>=3.0,<4.0',
+        'yarl>1.0,<2.0',
+        'idna-ssl>=1.0; python_version<"3.7"',
+        'typing_extensions>=3.6.5'
+    ]
 
 
 recipe = AIOHTTPRecipe()
